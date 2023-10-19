21f54f97b9b8beccb6b3b884198ee8ffe81c85f1
Luan <luantavres23@gmail.com>
2023-10-18 22:17:06 -0300

    primeiro comit

---
+++ src/main/java/com/example/primeiroprojeto/primeiroprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiroprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class primeiroprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(primeiroprojetoApplication.class, args);
+	}
+
+}
